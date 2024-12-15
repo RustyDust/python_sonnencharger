e3cb56a6798c4c16baf2fdd17712b2c267311012
stefan <stefan@whocares.de>
2024-12-15 11:56:37 +0100

    Switch to Async modbus client, new version 0.3.0
    Signed-off-by: stefan <stefan@whocares.de>

--- sonnencharger/__init__.py
+++ sonnencharger/__init__.py
@@ -1,3 +1,3 @@
 from .sonnencharger import sonnencharger
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
--- sonnencharger/sonnencharger.py
+++ sonnencharger/sonnencharger.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import traceback
 
@@ -56,88 +57,95 @@ class sonnencharger:
     pymodbus_apply_logging_config("ERROR")
     self._sc_host = ipaddress
     self._sc_port = port
-    self._sc_conn = ModbusClient.ModbusTcpClient(
-      host=self._sc_host,
-      port=self._sc_port,
-      framer=FramerType.SOCKET,
-    )
-    self._sc_conn.connect()
     self.system_data = {}
 
-  def _decode_string(self, offset, length):
+  async def _decode_string(self, offset, length):
     try:
-      rsp = self._sc_conn.read_input_registers(address=offset, count=length)
+      rsp = await self._sc_conn.read_input_registers(address=offset, count=length)
       return self._sc_conn.convert_from_registers(rsp.registers, self._sc_conn.DATATYPE.STRING).rstrip('\x00')
     except ModbusException as exc:
       e = traceback.format_exc()
       LOGGER.error(f"Unable to read charger value. [{exc}]\n{e}")
       return ""
 
-  def _decode_uint8(self, offset):
+  async def _decode_uint8(self, offset):
     try:
-      rsp = self._sc_conn.read_input_registers(address=offset, count=1)
+      rsp = await self._sc_conn.read_input_registers(address=offset, count=1)
       return self._sc_conn.convert_from_registers(rsp.registers, self._sc_conn.DATATYPE.UINT16)
     except ModbusException as exc:
       e = traceback.format_exc()
       LOGGER.error(f"Unable to read charger value. [{exc}]\n{e}")
       return False
 
-  def _decode_uint16(self, offset):
+  async def _decode_uint16(self, offset):
     try:
-      rsp = self._sc_conn.read_input_registers(address=offset, count=2)
+      rsp = await self._sc_conn.read_input_registers(address=offset, count=2)
       return self._sc_conn.convert_from_registers(rsp.registers, self._sc_conn.DATATYPE.UINT32)
     except ModbusException as exc:
       e = traceback.format_exc()
       LOGGER.error(f"Unable to read charger value. [{exc}]\n{e}")
       return False
 
-  def _decode_uint64(self, offset):
+  async def _decode_uint64(self, offset):
     try:
-      rsp = self._sc_conn.read_input_registers(address=offset, count=4)
+      rsp = await self._sc_conn.read_input_registers(address=offset, count=4)
       return self._sc_conn.convert_from_registers(rsp.registers, self._sc_conn.DATATYPE.UINT64)
     except ModbusException as exc:
       e = traceback.format_exc()
       LOGGER.error(f"Unable to read charger value. [{exc}]\n{e}")
       return False
 
-  def _decode_float32(self, offset):
+  async def _decode_float32(self, offset):
     try:
-      rsp = self._sc_conn.read_input_registers(address=offset, count=2)
+      rsp = await self._sc_conn.read_input_registers(address=offset, count=2)
       return self._sc_conn.convert_from_registers(rsp.registers, self._sc_conn.DATATYPE.FLOAT32)
     except ModbusException as exc:
       e = traceback.format_exc()
       LOGGER.error(f"Unable to read charger value. [{exc}]\n{e}")
       return False
 
-  def _collect_sysinfo(self):
-    self._sc_conn.connect()
+  async def _collect_sysinfo(self):
+    self._sc_conn = ModbusClient.AsyncModbusTcpClient(
+      host=self._sc_host,
+      port=self._sc_port,
+      framer=FramerType.SOCKET,
+    )
+    await self._sc_conn.connect()
     # Serial Number
-    serial = self._decode_string(ADDR_SC_SERIALNO, 10)
+    serial = await self._decode_string(ADDR_SC_SERIALNO, 10)
     self.system_data['serial'] = serial
 
     # Model
-    model = self._decode_string(ADDR_SC_MODEL, 10)
+    model = await self._decode_string(ADDR_SC_MODEL, 10)
     self.system_data['model'] = model
 
     # HW version
-    hwrev = self._decode_string(ADDR_SC_HWREV, 5)
+    hwrev = await self._decode_string(ADDR_SC_HWREV, 5)
     self.system_data['hwrevision'] = hwrev
 
     # SW version
-    swrev = self._decode_string(ADDR_SC_SWREV, 5)
+    swrev = await self._decode_string(ADDR_SC_SWREV, 5)
     self.system_data['swrevision'] = swrev
 
     # no# of connectors
-    conns = self._decode_uint8(ADDR_SC_NUMCONN)
+    conns = await self._decode_uint8(ADDR_SC_NUMCONN)
     self.system_data['connectors'] = conns
 
-  def _collect_connector_info(self):
+    self._sc_conn.close()
+
+  async def _collect_connector_info(self):
+    self._sc_conn = ModbusClient.AsyncModbusTcpClient(
+      host=self._sc_host,
+      port=self._sc_port,
+      framer=FramerType.SOCKET,
+    )
+    await self._sc_conn.connect()
     self.connector_data = {}
     for c in range(self.system_data['connectors']):
       offset = c * 100
       connector = {}
       # connector state
-      conn_state = self._decode_uint8(ADDR_CONN_STATE)
+      conn_state = await self._decode_uint8(ADDR_CONN_STATE)
       connector['state_numeric'] = conn_state
 
       if conn_state == 1:
@@ -164,7 +172,7 @@ class sonnencharger:
         connector['state'] = "Unknown state!"
 
       # Vehicle connected phase
-      conn_phs = self._decode_uint8(ADDR_CONN_VHCL_PHS)
+      conn_phs = await self._decode_uint8(ADDR_CONN_VHCL_PHS)
       connector['vehicle_connected_phases_code'] = conn_phs
 
       if conn_phs == 0:
@@ -190,67 +198,67 @@ class sonnencharger:
         connector['vehicle_connected_phases_numeric'] = -1
 
       # ev max phase current (limited to 3 digits)
-      connector['ev_max_phase_current'] = round(self._decode_float32(ADDR_CONN_VHCL_MAX_CURR), 3)
+      connector['ev_max_phase_current'] = round(await self._decode_float32(ADDR_CONN_VHCL_MAX_CURR), 3)
 
       # target current (limited to 3 digits)
-      connector['target_current'] = round(self._decode_float32(ADDR_CONN_VHCL_TGT_CURR), 3)
+      connector['target_current'] = round(await self._decode_float32(ADDR_CONN_VHCL_TGT_CURR), 3)
 
       # net frequency
-      connector['net_frequency'] = round(self._decode_float32(ADDR_CONN_NET_FREQUENCY), 3)
+      connector['net_frequency'] = round(await self._decode_float32(ADDR_CONN_NET_FREQUENCY), 3)
 
       # l1 L-N voltage
-      connector['l1_ln_voltage'] = round(self._decode_float32(ADDR_CONN_L1_VOLTAGE), 3)
+      connector['l1_ln_voltage'] = round(await self._decode_float32(ADDR_CONN_L1_VOLTAGE), 3)
 
       # l2 L-N voltage
-      connector['l2_ln_voltage'] = round(self._decode_float32(ADDR_CONN_L2_VOLTAGE), 3)
+      connector['l2_ln_voltage'] = round(await self._decode_float32(ADDR_CONN_L2_VOLTAGE), 3)
 
       # l3 L-N voltage
-      connector['l3_ln_voltage'] = round(self._decode_float32(ADDR_CONN_L3_VOLTAGE), 3)
+      connector['l3_ln_voltage'] = round(await self._decode_float32(ADDR_CONN_L3_VOLTAGE), 3)
 
       # l1 current
-      connector['l1_current'] = round(self._decode_float32(ADDR_CONN_L1_CURRNT), 3)
+      connector['l1_current'] = round(await self._decode_float32(ADDR_CONN_L1_CURRNT), 3)
 
       # l2 current
-      connector['l2_current'] = round(self._decode_float32(ADDR_CONN_L2_CURRNT), 3)
+      connector['l2_current'] = round(await self._decode_float32(ADDR_CONN_L2_CURRNT), 3)
 
       # l3 current
-      connector['l3_current'] = round(self._decode_float32(ADDR_CONN_L3_CURRNT), 3)
+      connector['l3_current'] = round(await self._decode_float32(ADDR_CONN_L3_CURRNT), 3)
 
       # l1 active power
-      connector['l1_active_power'] = round(self._decode_float32(ADDR_CONN_L1_ACT_PWR), 3)
+      connector['l1_active_power'] = round(await self._decode_float32(ADDR_CONN_L1_ACT_PWR), 3)
 
       # l2 active power
-      connector['l2_active_power'] = round(self._decode_float32(ADDR_CONN_L2_ACT_PWR), 3)
+      connector['l2_active_power'] = round(await self._decode_float32(ADDR_CONN_L2_ACT_PWR), 3)
 
       # l2 active power
-      connector['l3_active_power'] = round(self._decode_float32(ADDR_CONN_L3_ACT_PWR), 3)
+      connector['l3_active_power'] = round(await self._decode_float32(ADDR_CONN_L3_ACT_PWR), 3)
 
       # total active power
-      connector['total_active_power'] = round(self._decode_float32(ADDR_CONN_TOT_ACT_PWR), 3)
+      connector['total_active_power'] = round(await self._decode_float32(ADDR_CONN_TOT_ACT_PWR), 3)
 
       # power factor
-      connector['power_factor'] = round(self._decode_float32(ADDR_CONN_PWR_FACTOR), 3)
+      connector['power_factor'] = round(await self._decode_float32(ADDR_CONN_PWR_FACTOR), 3)
 
       # impoerted energy in running session (kWh)
-      connector['active_session_imported_energy'] = round(self._decode_float32(ADDR_CONN_SESS_NRGY_IMP), 3)
+      connector['active_session_imported_energy'] = round(await self._decode_float32(ADDR_CONN_SESS_NRGY_IMP), 3)
 
       # session duration (s)
-      connector['active_session_duration'] = self._decode_uint64(ADDR_CONN_SESS_DURATION)
+      connector['active_session_duration'] = await self._decode_uint64(ADDR_CONN_SESS_DURATION)
 
       # session departure timee (s)
-      connector['session_departure_time'] = self._decode_uint64(ADDR_CONN_SESS_DEPART)
+      connector['session_departure_time'] = await self._decode_uint64(ADDR_CONN_SESS_DEPART)
 
       # running session id
-      connector['session_id'] = self._decode_uint64(ADDR_CONN_SESS_ID)
+      connector['session_id'] = await self._decode_uint64(ADDR_CONN_SESS_ID)
 
       # ev max power
-      connector['ev_max_power'] = round(self._decode_float32(ADDR_CONN_VHCL_MAX_PWR), 3)
+      connector['ev_max_power'] = round(await self._decode_float32(ADDR_CONN_VHCL_MAX_PWR), 3)
 
       # ev max power
-      connector['ev_required_energy'] = round(self._decode_float32(ADDR_CONN_VHCL_REQ_NRGY), 3)
+      connector['ev_required_energy'] = round(await self._decode_float32(ADDR_CONN_VHCL_REQ_NRGY), 3)
 
       # connector type
-      conn_type = self._decode_uint8(ADDR_CONN_TYPE + offset)
+      conn_type = await self._decode_uint8(ADDR_CONN_TYPE + offset)
       connector['type_numeric'] = conn_type
       if conn_type == 1:
         connector['type'] = "Socket Type 2"
@@ -260,22 +268,27 @@ class sonnencharger:
         connector['type'] = "Unknown connector type"
 
       # noumber of phases
-      connector['num_phases'] = self._decode_uint8(ADDR_CONN_NUM_PHS + offset)
+      connector['num_phases'] = await self._decode_uint8(ADDR_CONN_NUM_PHS + offset)
 
       # pase 1 connected
       for p in range(1,4):
-        connector['l{}_phase'.format(p)] = self._decode_uint16(ADDR_CONN_NUM_PHS + p + offset)
+        connector['l{}_phase'.format(p)] = await self._decode_uint16(ADDR_CONN_NUM_PHS + p + offset)
 
       # max current
-      connector['max_current'] = self._decode_float32(ADDR_CONN_MAX_CURR + offset)
+      connector['max_current'] = await self._decode_float32(ADDR_CONN_MAX_CURR + offset)
 
       self.connector_data[c] = connector
+      self._sc_conn.close()
 
   # General system info
   def get_sysinfo(self):
-    self._collect_sysinfo()
+    asyncio.run(
+      self._collect_sysinfo()
+    )
     return self.system_data
 
   def get_connectors(self):
-    self._collect_connector_info()
+    asyncio.run(
+      self._collect_connector_info()
+    )
     return self.connector_data
